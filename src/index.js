9a29ee18bb49bb6c09fbb7db94b415d436aa3000
Akash <vv.akku111@gmail.com>
2024-10-15 18:31:17 +0530

    last modified code with error

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// import dotenv from "dotenv";
+// import connectDB from "./db/index.js";
+
+// dotenv.config({
+//     path: './env'
+// })
+
+// connectDB()
+
+// index.mjs
+import connectDB from "./db/index.js";
+
+// Start the connection
+(async () => {
+  await connectDB();
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async () =>{
+    try {
+        await mongoose.connect(`${process.env.
+        MONGODB_URI}/${DB_NAME}`)
+        app.on('error',(error)=>{
+            console.log("Errr:", error);
+            throw error
+            
+        })
+
+    }catch(error){
+        console.error("Error: ", error);
+        throw err
+    }
+}) ()  
+*/
